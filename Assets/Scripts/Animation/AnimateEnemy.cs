37addd9d50d31fd8e4930de0ba56dad025a43762
mitsuha <usoppgod2003@gmail.com>
2024-09-25 20:16:53 +0700

    fix some things

--- Assets/Scripts/Animation/AnimateEnemy.cs
+++ Assets/Scripts/Animation/AnimateEnemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimateEnemyMomoSlime : MonoBehaviour
+public class AnimateEnemy : MonoBehaviour
 {
     private Enemy enemy;
     private AimDirection aimDirection;
